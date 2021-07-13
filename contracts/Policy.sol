8857b8169cd434ec7751acce0e71251b803e5c88
Guillermo M. Narvaja <guillermo.narvaja@radiocut.fm>
2021-07-13 18:32:18 -0300

    Updated README and license changed to Apache2
    Added also some additional files as CODE_OF_CONDUCT LICENSE and others.

--- contracts/Policy.sol
+++ contracts/Policy.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 import {WadRayMath} from './WadRayMath.sol';
 import {IRiskModule} from '../interfaces/IRiskModule.sol';
