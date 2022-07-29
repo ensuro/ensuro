db07563c1dc716e1d528b827fad8a4dfaaa068f3
GnP <g.parrondo@gmail.com>
2022-07-29 14:15:39 -0300

    Make the linter happy

--- contracts/mocks/ERC20Faucet.sol
+++ contracts/mocks/ERC20Faucet.sol
@@ -3,37 +3,37 @@ pragma solidity ^0.8.3;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Faucet {
-    ERC20 public token;
+  ERC20 public token;
 
-    address public owner;
+  address public owner;
 
-    // units of token allowed for tapping into the faucet
-    uint256 public dailyLimit;
+  // units of token allowed for tapping into the faucet
+  uint256 public dailyLimit;
 
-    mapping(address => uint) public lockTime;
+  mapping(address => uint256) public lockTime;
 
-    event Tapped(address from, uint256 amount);
+  event Tapped(address from, uint256 amount);
 
-    constructor(address _token, uint256 _dailyLimit) {
-        token = ERC20(_token);
-        owner = msg.sender;
-        dailyLimit = _dailyLimit;
-    }
+  constructor(address _token, uint256 _dailyLimit) {
+    token = ERC20(_token);
+    owner = msg.sender;
+    dailyLimit = _dailyLimit;
+  }
 
-    function totalSupply() external view returns (uint256) {
-        return token.balanceOf(address(this));
-    }
+  function totalSupply() external view returns (uint256) {
+    return token.balanceOf(address(this));
+  }
 
-    function tap() public {
-        require(block.timestamp > lockTime[msg.sender], "No soup for you!");
-        uint8 decimals = token.decimals();
-        uint256 amount = dailyLimit * (10**decimals);
-        require(this.totalSupply() >= amount, "Insufficient supply");
+  function tap() public {
+    require(block.timestamp > lockTime[msg.sender], "No soup for you!");
+    uint8 decimals = token.decimals();
+    uint256 amount = dailyLimit * (10**decimals);
+    require(this.totalSupply() >= amount, "Insufficient supply");
 
-        lockTime[msg.sender] = block.timestamp + 1 days;
-        
-        token.transfer(msg.sender, amount);
+    lockTime[msg.sender] = block.timestamp + 1 days;
 
-        emit Tapped(msg.sender, dailyLimit);
-    }
+    token.transfer(msg.sender, amount);
+
+    emit Tapped(msg.sender, dailyLimit);
+  }
 }
