4cafa9eb4a3dd6dbd3ffd318490ec676e2ef6a9e
Guillermo M. Narvaja <guillermo.narvaja@radiocut.fm>
2022-09-02 04:23:23 -0300

    Fix deploy and test scripts.

--- test/test-utils.js
+++ test/test-utils.js
@@ -139,7 +139,7 @@ exports.getTransactionEvent = function(interface, receipt, eventName) {
 
 exports.deployPool = async function(hre, options) {
   const PolicyPool = await ethers.getContractFactory("PolicyPool");
-  const PolicyPoolConfig = await ethers.getContractFactory("PolicyPoolConfig");
+  const AccessManager = await ethers.getContractFactory("AccessManager");
   const PolicyNFT = await ethers.getContractFactory("PolicyNFT");
 
   // Deploy PolicyNFT
@@ -154,18 +154,18 @@ exports.deployPool = async function(hre, options) {
   );
   await policyNFT.deployed();
 
-  // Deploy PolicyPoolConfig
-  const policyPoolConfig = await hre.upgrades.deployProxy(PolicyPoolConfig, [
+  // Deploy AccessManager
+  const accessManager = await hre.upgrades.deployProxy(AccessManager, [
     options.policyPoolDetAddress || ethers.constants.AddressZero,
   ], {kind: 'uups'});
 
-  await policyPoolConfig.deployed();
+  await accessManager.deployed();
 
   const policyPool = await hre.upgrades.deployProxy(
     PolicyPool,
     [options.treasuryAddress || ethers.constants.AddressZero],
     {
-      constructorArgs: [policyPoolConfig.address, policyNFT.address, options.currency],
+      constructorArgs: [accessManager.address, policyNFT.address, options.currency],
       kind: 'uups',
       unsafeAllow: ["delegatecall"],
     }
@@ -174,12 +174,12 @@ exports.deployPool = async function(hre, options) {
   await policyPool.deployed();
 
   for (const role of (options.grantRoles || [])) {
-    await grantRole(hre, policyPoolConfig, role);
+    await grantRole(hre, accessManager, role);
   }
 
-  await grantRole(hre, policyPoolConfig, "LEVEL1_ROLE");
-  await grantRole(hre, policyPoolConfig, "LEVEL2_ROLE");
-  await grantRole(hre, policyPoolConfig, "LEVEL3_ROLE");
+  await grantRole(hre, accessManager, "LEVEL1_ROLE");
+  await grantRole(hre, accessManager, "LEVEL2_ROLE");
+  await grantRole(hre, accessManager, "LEVEL3_ROLE");
   return policyPool;
 }
 
