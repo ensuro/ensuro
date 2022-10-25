a9719be0f81b08a790355ca25832aaf2bea69a96
GnP <gabriel@ensuro.co>
2022-10-25 09:47:05 -0300

    Add test for storage gaps

--- scripts/storageLayout.js
+++ scripts/storageLayout.js
@@ -1,6 +1,7 @@
 const hre = require("hardhat");
 const { artifacts } = require("hardhat");
 const { findAll } = require("solidity-ast/utils");
+const { getStorageLayout } = require("../test/test-utils");
 
 /**
  * Script to print a contracts storage layout as a table.
@@ -10,42 +11,23 @@ const { findAll } = require("solidity-ast/utils");
 
 //
 
-async function getStorageLayout(contractSrc, contractName) {
-  const buildInfo = await artifacts.getBuildInfo(`${contractSrc}:${contractName}`);
-  const solcOutput = buildInfo.output;
-
-  const contracts = {};
-  const storageLayouts = {};
-
-  for (const def of findAll("ContractDefinition", solcOutput.sources[contractSrc].ast)) {
-    contracts[def.name] = def;
-    storageLayouts[def.name] = solcOutput.contracts[contractSrc][def.name].storageLayout;
-  }
-
-  return storageLayouts[contractName].storage;
-}
-
 async function main(contractSrc, contractName) {
   await hre.run("compile");
   const layout = await getStorageLayout(contractSrc, contractName);
 
-  const summary = layout.map((item) => ({
-    slot: item.slot,
+  const summary = layout.storage.map((item) => ({
+    slot: parseInt(item.slot),
     label: item.label,
     type: item.type,
+    numberOfBytes: parseInt(layout.types[item.type].numberOfBytes),
+    endSlot: parseInt(item.slot) + Math.floor(parseInt(layout.types[item.type].numberOfBytes) / 32),
   }));
   console.table(summary);
 }
 
-function getContractSourceName(name) {
-  return [`contracts/${name}.sol`, name];
-}
+const [contractSrc, contractName] = [`contracts/${process.argv[2]}.sol`, process.argv[2]];
 
-const [contractSrc, contractName] = getContractSourceName(process.argv[2]);
-
-console.log(`# Storage layout for ${contractSrc}:${contractName}`);
-console.log("```");
+console.log(`Storage layout for ${contractSrc}:${contractName}`);
 main(contractSrc, contractName).then(() => {
-  console.log("```");
   process.exit(0);
 });
---
+++ test/test-storage-gaps.js
@@ -0,0 +1,35 @@
+require("mocha");
+const { expect } = require("chai");
+const hre = require("hardhat");
+
+const { getStorageLayout } = require("./test-utils");
+
+describe("Storage Gaps", () => {
+  const contracts = [
+    "EToken",
+    "AccessManager",
+    "LPManualWhitelist",
+    "PolicyPool",
+    "PolicyPoolComponent",
+    "PremiumsAccount",
+    "Reserve",
+    "RiskModule",
+    "SignedQuoteRiskModule",
+    "TrustfulRiskModule",
+  ];
+
+  for (const contract of contracts) {
+    it(`${contract} has a proper storage gap`, async () => {
+      const { storage, types } = await getStorageLayout(`contracts/${contract}.sol`, contract);
+
+      const gap = storage[storage.length - 1];
+
+      // Check the storage ends with a gap
+      expect(gap.label).to.equal("__gap");
+
+      // Check the storage aligns to 50 slots (+1 because of https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/issues/182)
+      const finalSlot = parseInt(gap.slot) + Math.floor(parseInt(types[gap.type].numberOfBytes) / 32);
+      expect(finalSlot % 50).to.be.lessThanOrEqual(1);
+    });
+  }
+});
