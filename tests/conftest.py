57551bee3cf74b4a03da40956126402c080d15ee
GnP <gabriel@ensuro.co>
2024-02-22 07:38:03 -0300

    Set gasPrice to zero on test startup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,17 @@
 import pytest
-from ethproto import wrappers
+from ethproto import w3wrappers, wrappers
+from web3 import Web3
 
 
 def pytest_configure(config):
     wrappers.DEFAULT_PROVIDER = "w3"
+    w3wrappers.CONTRACT_JSON_PATH = ["artifacts"]
 
 
 @pytest.fixture(scope="module", autouse=True)
 def reset_provider():
     """Resets the provider for each module. Mainly for addressbook and contract map cleanse"""
-    from ethproto.w3wrappers import W3Provider
-    from web3 import Web3
 
-    wrappers.register_provider("w3", W3Provider(Web3()))
+    wrappers.register_provider("w3", w3wrappers.W3Provider(Web3(), tx_kwargs={"gasPrice": 0}))
     yield
-    wrappers.register_provider("w3", W3Provider(Web3()))
+    wrappers.register_provider("w3", w3wrappers.W3Provider(Web3(), tx_kwargs={"gasPrice": 0}))
