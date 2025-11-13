04f019468439b54d94877f8b22392844ae7c7efc
Guillermo M. Narvaja <guillermo.narvaja@radiocut.fm>
2025-11-13 13:53:52 -0300

    Unit tests for the underwriters

--- js/utils.js
+++ js/utils.js
@@ -239,6 +239,11 @@ function makeFTUWInputData({ payout, premium, lossProb, expiration, internalId, 
   );
 }
 
+async function makeAndSignFTUWInputData({ payout, premium, lossProb, expiration, internalId, params, signer }) {
+  const input = makeFTUWInputData({ payout, premium, lossProb, expiration, internalId, params });
+  return ethers.concat([input, await signer.signMessage(ethers.getBytes(input))]);
+}
+
 function makeFTUWReplacementInputData({ oldPolicy, payout, premium, lossProb, expiration, internalId, params }) {
   return abiCoder.encode(
     [
@@ -254,6 +259,20 @@ function makeFTUWReplacementInputData({ oldPolicy, payout, premium, lossProb, ex
   );
 }
 
+async function makeAndSignFTUWReplacementInputData({
+  oldPolicy,
+  payout,
+  premium,
+  lossProb,
+  expiration,
+  internalId,
+  params,
+  signer,
+}) {
+  const input = makeFTUWReplacementInputData({ oldPolicy, payout, premium, lossProb, expiration, internalId, params });
+  return ethers.concat([input, await signer.signMessage(ethers.getBytes(input))]);
+}
+
 function makeFTUWCancelInputData({ policyToCancel, purePremiumRefund, jrCocRefund, srCocRefund }) {
   return abiCoder.encode(
     [
@@ -264,6 +283,11 @@ function makeFTUWCancelInputData({ policyToCancel, purePremiumRefund, jrCocRefun
     ],
     [policyToCancel, purePremiumRefund, jrCocRefund, srCocRefund]
   );
+}
+
+async function makeAndSignFTUWCancelInputData({ policyToCancel, purePremiumRefund, jrCocRefund, srCocRefund, signer }) {
+  const input = makeFTUWCancelInputData({ policyToCancel, purePremiumRefund, jrCocRefund, srCocRefund });
+  return ethers.concat([input, await signer.signMessage(ethers.getBytes(input))]);
 }
 
 /**
@@ -281,6 +305,13 @@ function makeWhitelistStatus(status) {
     );
 }
 
+/**
+ * Returns a bytes4 selector from a string. Equivalent to Solidity `bytes4(keccak256(name))`
+ */
+function makeHashSelector(name) {
+  return ethers.keccak256(ethers.toUtf8Bytes(name)).slice(0, 10);
+}
+
 module.exports = {
   defaultPolicyParams,
   defaultPolicyParamsWithBucket,
@@ -288,8 +319,12 @@ module.exports = {
   defaultBucketParams,
   defaultTestParams,
   makeFTUWInputData,
+  makeAndSignFTUWInputData,
   makeFTUWReplacementInputData,
+  makeAndSignFTUWReplacementInputData,
   makeFTUWCancelInputData,
+  makeAndSignFTUWCancelInputData,
+  makeHashSelector,
   makeBucketQuoteMessage,
   makeFullQuoteMessage,
   paramsAsUint256,
---
+++ test/test-underwriters.js
@@ -0,0 +1,379 @@
+const { expect } = require("chai");
+const hre = require("hardhat");
+const helpers = require("@nomicfoundation/hardhat-network-helpers");
+
+const { amountFunction, _W, getAccessManagerRole } = require("@ensuro/utils/js/utils");
+const { DAY } = require("@ensuro/utils/js/constants");
+const { initCurrency } = require("@ensuro/utils/js/test-utils");
+const { getAccessManager } = require("@ensuro/access-managed-proxy/js/deployProxy");
+const { addEToken, deployPool, deployPremiumsAccount, addRiskModule } = require("../js/test-utils");
+const {
+  makeFTUWInputData,
+  makeAndSignFTUWInputData,
+  defaultBucketParams,
+  makeFTUWReplacementInputData,
+  makeFTUWCancelInputData,
+  makeHashSelector,
+  makeAndSignFTUWReplacementInputData,
+  makeAndSignFTUWCancelInputData,
+} = require("../js/utils");
+const { ethers } = hre;
+
+const _A = amountFunction(6);
+const { ZeroAddress, MaxUint256 } = hre.ethers;
+const SIGNATURE_SIZE = 65;
+
+async function fullTrustedUWFixture() {
+  const FullTrustedUW = await hre.ethers.getContractFactory("FullTrustedUW");
+  const uw = await FullTrustedUW.deploy();
+
+  return { uw };
+}
+
+async function fullSignedUWFixture() {
+  const [owner, other, signer] = await hre.ethers.getSigners();
+  const currency = await initCurrency(
+    { name: "Test USDC", symbol: "USDC", decimals: 6, initial_supply: _A(10000) },
+    [owner],
+    [_A(5000)]
+  );
+
+  const pool = await deployPool({
+    currency: currency,
+    treasuryAddress: await ethers.resolveAddress(other),
+  });
+  pool._A = _A;
+
+  const srEtk = await addEToken(pool, {});
+  const premiumsAccount = await deployPremiumsAccount(pool, { srEtk });
+  const FullSignedUW = await hre.ethers.getContractFactory("FullSignedUW");
+  const uw = await FullSignedUW.deploy();
+
+  const rm = await addRiskModule(pool, premiumsAccount, {
+    underwriter: uw,
+    extraArgs: [],
+  });
+
+  const acMgr = await getAccessManager(pool);
+  return { uw, owner, rm, acMgr, other, signer };
+}
+
+function randint(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function randAmount(max = 1000) {
+  return _A(randint(max));
+}
+
+function randWad(add = 0) {
+  return _W(add + randint(1000) / 1000);
+}
+
+function makeRandomPriceNewInput() {
+  return {
+    payout: randAmount(),
+    premium: randAmount(),
+    lossProb: randWad(),
+    expiration: 1762978903 + randint(1000),
+    internalId: randint(1000),
+    params: defaultBucketParams({
+      moc: randWad(1),
+      jrCollRatio: randWad(),
+      collRatio: randWad(),
+      ensuroPpFee: randWad(),
+      ensuroCocFee: randWad(),
+      jrRoc: randWad(),
+      srRoc: randWad(),
+    }),
+  };
+}
+
+function makeRandomPolicy() {
+  return {
+    id: randint(100000000),
+    payout: randAmount(),
+    jrScr: randAmount(),
+    srScr: randAmount(),
+    lossProb: randWad(),
+    purePremium: randAmount(),
+    ensuroCommission: randAmount(),
+    partnerCommission: randAmount(),
+    jrCoc: randAmount(),
+    srCoc: randAmount(),
+    start: 1762978903 + randint(1000),
+    expiration: 1762978903 + randint(1000),
+    asParams: function () {
+      return [
+        this.id,
+        this.payout,
+        this.jrScr,
+        this.srScr,
+        this.lossProb,
+        this.purePremium,
+        this.ensuroCommission,
+        this.partnerCommission,
+        this.jrCoc,
+        this.srCoc,
+        this.start,
+        this.expiration,
+      ];
+    },
+  };
+}
+
+function makeRandomPriceReplaceInput() {
+  return {
+    oldPolicy: makeRandomPolicy().asParams(),
+    ...makeRandomPriceNewInput(),
+  };
+}
+
+function makeRandomPriceCancellationInput() {
+  return {
+    policyToCancel: makeRandomPolicy().asParams(),
+    purePremiumRefund: randAmount(),
+    jrCocRefund: randAmount(),
+    srCocRefund: randAmount(),
+  };
+}
+
+describe("FullTrustedUW", () => {
+  it("Checks it decodes the same input for priceNewPolicy", async () => {
+    const { uw } = await helpers.loadFixture(fullTrustedUWFixture);
+
+    for (let i = 0; i < 100; i++) {
+      const input = makeRandomPriceNewInput();
+      const output = await uw.priceNewPolicy(ZeroAddress, makeFTUWInputData(input));
+      expect(output.length).to.equal(6);
+      expect(output[0]).to.equal(input.payout);
+      expect(output[1]).to.equal(input.premium);
+      expect(output[2]).to.equal(input.lossProb);
+      expect(output[3]).to.equal(input.expiration);
+      expect(output[4]).to.equal(input.internalId);
+      expect(output[5]).to.deep.equal(input.params.asParams());
+    }
+  });
+
+  it("Checks it decodes the same input for pricePolicyReplacement", async () => {
+    const { uw } = await helpers.loadFixture(fullTrustedUWFixture);
+
+    for (let i = 0; i < 100; i++) {
+      const input = makeRandomPriceReplaceInput();
+      const output = await uw.pricePolicyReplacement(ZeroAddress, makeFTUWReplacementInputData(input));
+      expect(output.length).to.equal(7);
+      expect(output[0]).to.deep.equal(input.oldPolicy);
+      expect(output[1]).to.equal(input.payout);
+      expect(output[2]).to.equal(input.premium);
+      expect(output[3]).to.equal(input.lossProb);
+      expect(output[4]).to.equal(input.expiration);
+      expect(output[5]).to.equal(input.internalId);
+      expect(output[6]).to.deep.equal(input.params.asParams());
+    }
+  });
+
+  it("Checks it decodes the same input for pricePolicyCancellation", async () => {
+    const { uw } = await helpers.loadFixture(fullTrustedUWFixture);
+
+    for (let i = 0; i < 100; i++) {
+      const input = makeRandomPriceCancellationInput();
+      const output = await uw.pricePolicyCancellation(ZeroAddress, makeFTUWCancelInputData(input));
+      expect(output.length).to.equal(4);
+      expect(output[0]).to.deep.equal(input.policyToCancel);
+      expect(output[1]).to.equal(input.purePremiumRefund);
+      expect(output[2]).to.equal(input.jrCocRefund);
+      expect(output[3]).to.equal(input.srCocRefund);
+    }
+  });
+
+  it("Checks pricePolicyCancellation extracts the accrued interest when receices MaxUint256", async () => {
+    const { uw } = await helpers.loadFixture(fullTrustedUWFixture);
+    const now = await helpers.time.latest();
+
+    for (let i = 0; i < 10; i++) {
+      const input = makeRandomPriceCancellationInput();
+      input.policyToCancel[10] = now - DAY;
+      input.policyToCancel[11] = now + 9 * DAY;
+      input.jrCocRefund = MaxUint256;
+      const output = await uw.pricePolicyCancellation(ZeroAddress, makeFTUWCancelInputData(input));
+      expect(output.length).to.equal(4);
+      expect(output[0]).to.deep.equal(input.policyToCancel);
+      expect(output[1]).to.equal(input.purePremiumRefund);
+      expect(output[2]).not.to.equal(input.jrCocRefund);
+      expect(output[2]).to.closeTo((input.policyToCancel[8] * 9n) / 10n, 100n);
+      expect(output[3]).to.equal(input.srCocRefund);
+    }
+
+    for (let i = 0; i < 10; i++) {
+      const input = makeRandomPriceCancellationInput();
+      input.policyToCancel[10] = now - DAY;
+      input.policyToCancel[11] = now + 9 * DAY;
+      input.srCocRefund = MaxUint256;
+      const output = await uw.pricePolicyCancellation(ZeroAddress, makeFTUWCancelInputData(input));
+      expect(output.length).to.equal(4);
+      expect(output[0]).to.deep.equal(input.policyToCancel);
+      expect(output[1]).to.equal(input.purePremiumRefund);
+      expect(output[2]).to.equal(input.jrCocRefund);
+      expect(output[3]).not.to.equal(input.srCocRefund);
+      expect(output[3]).to.closeTo((input.policyToCancel[9] * 9n) / 10n, 100n);
+    }
+  });
+});
+
+describe("FullSignedUW", () => {
+  it("Checks it decodes the same input for priceNewPolicy", async () => {
+    const { uw, acMgr, rm, signer, other } = await helpers.loadFixture(fullSignedUWFixture);
+
+    const FULL_NEW_POLICY_SIGNER = getAccessManagerRole("FULL_NEW_POLICY_SIGNER");
+    const operationSelector = makeHashSelector("FULL_PRICE_NEW_POLICY");
+
+    await acMgr.setTargetFunctionRole(rm, [operationSelector], FULL_NEW_POLICY_SIGNER);
+    await acMgr.grantRole(FULL_NEW_POLICY_SIGNER, signer, 0);
+
+    for (let i = 0; i < 5; i++) {
+      const input = makeRandomPriceNewInput();
+
+      // Send wrong input
+      await expect(uw.priceNewPolicy(ZeroAddress, makeFTUWInputData(input)))
+        .to.be.revertedWithCustomError(uw, "InvalidInputSize")
+        .withArgs(
+          ethers.getBytes(makeFTUWInputData(input)).length,
+          ethers.getBytes(makeFTUWInputData(input)).length + SIGNATURE_SIZE
+        );
+      // Send correct input, but ZeroAddress as RM
+      await expect(
+        uw.priceNewPolicy(ZeroAddress, await makeAndSignFTUWInputData({ signer, ...input }))
+      ).to.be.revertedWithoutReason();
+      // Send correct input, rm but invalid signer
+      await expect(uw.priceNewPolicy(rm, await makeAndSignFTUWInputData({ signer: other, ...input })))
+        .to.be.revertedWithCustomError(uw, "UnauthorizedSigner")
+        .withArgs(other, operationSelector);
+      const output = await uw.priceNewPolicy(rm, await makeAndSignFTUWInputData({ signer, ...input }));
+      expect(output.length).to.equal(6);
+      expect(output[0]).to.equal(input.payout);
+      expect(output[1]).to.equal(input.premium);
+      expect(output[2]).to.equal(input.lossProb);
+      expect(output[3]).to.equal(input.expiration);
+      expect(output[4]).to.equal(input.internalId);
+      expect(output[5]).to.deep.equal(input.params.asParams());
+    }
+  });
+
+  it("Checks it decodes the same input for pricePolicyReplacement", async () => {
+    const { uw, acMgr, rm, signer, other } = await helpers.loadFixture(fullSignedUWFixture);
+
+    const FULL_REPLACE_POLICY_SIGNER = getAccessManagerRole("FULL_REPLACE_POLICY_SIGNER");
+    const operationSelector = makeHashSelector("FULL_PRICE_REPLACE_POLICY");
+
+    await acMgr.setTargetFunctionRole(rm, [operationSelector], FULL_REPLACE_POLICY_SIGNER);
+    await acMgr.grantRole(FULL_REPLACE_POLICY_SIGNER, signer, 0);
+
+    for (let i = 0; i < 5; i++) {
+      const input = makeRandomPriceReplaceInput();
+      // Send wrong input
+      await expect(uw.pricePolicyReplacement(ZeroAddress, makeFTUWReplacementInputData(input)))
+        .to.be.revertedWithCustomError(uw, "InvalidInputSize")
+        .withArgs(
+          ethers.getBytes(makeFTUWReplacementInputData(input)).length,
+          ethers.getBytes(makeFTUWReplacementInputData(input)).length + SIGNATURE_SIZE
+        );
+      // Send correct input, but ZeroAddress as RM
+      await expect(
+        uw.pricePolicyReplacement(ZeroAddress, await makeAndSignFTUWReplacementInputData({ signer, ...input }))
+      ).to.be.revertedWithoutReason();
+      // Send correct input, rm but invalid signer
+      await expect(
+        uw.pricePolicyReplacement(rm, await makeAndSignFTUWReplacementInputData({ signer: other, ...input }))
+      )
+        .to.be.revertedWithCustomError(uw, "UnauthorizedSigner")
+        .withArgs(other, operationSelector);
+      const output = await uw.pricePolicyReplacement(
+        rm,
+        await makeAndSignFTUWReplacementInputData({ signer, ...input })
+      );
+      expect(output.length).to.equal(7);
+      expect(output[0]).to.deep.equal(input.oldPolicy);
+      expect(output[1]).to.equal(input.payout);
+      expect(output[2]).to.equal(input.premium);
+      expect(output[3]).to.equal(input.lossProb);
+      expect(output[4]).to.equal(input.expiration);
+      expect(output[5]).to.equal(input.internalId);
+      expect(output[6]).to.deep.equal(input.params.asParams());
+    }
+  });
+
+  it("Checks it decodes the same input for pricePolicyCancellation", async () => {
+    const { uw, acMgr, rm, signer, other } = await helpers.loadFixture(fullSignedUWFixture);
+
+    const FULL_CANCEL_POLICY_SIGNER = getAccessManagerRole("FULL_CANCEL_POLICY_SIGNER");
+    const operationSelector = makeHashSelector("FULL_PRICE_CANCEL_POLICY");
+
+    await acMgr.setTargetFunctionRole(rm, [operationSelector], FULL_CANCEL_POLICY_SIGNER);
+    await acMgr.grantRole(FULL_CANCEL_POLICY_SIGNER, signer, 0);
+
+    for (let i = 0; i < 5; i++) {
+      const input = makeRandomPriceCancellationInput();
+      // Send wrong input
+      await expect(uw.pricePolicyCancellation(ZeroAddress, makeFTUWCancelInputData(input)))
+        .to.be.revertedWithCustomError(uw, "InvalidInputSize")
+        .withArgs(
+          ethers.getBytes(makeFTUWCancelInputData(input)).length,
+          ethers.getBytes(makeFTUWCancelInputData(input)).length + SIGNATURE_SIZE
+        );
+      // Send correct input, but ZeroAddress as RM
+      await expect(
+        uw.pricePolicyCancellation(ZeroAddress, await makeAndSignFTUWCancelInputData({ signer, ...input }))
+      ).to.be.revertedWithoutReason();
+      // Send correct input, rm but invalid signer
+      await expect(uw.pricePolicyCancellation(rm, await makeAndSignFTUWCancelInputData({ signer: other, ...input })))
+        .to.be.revertedWithCustomError(uw, "UnauthorizedSigner")
+        .withArgs(other, operationSelector);
+      const output = await uw.pricePolicyCancellation(rm, await makeAndSignFTUWCancelInputData({ signer, ...input }));
+      expect(output.length).to.equal(4);
+      expect(output[0]).to.deep.equal(input.policyToCancel);
+      expect(output[1]).to.equal(input.purePremiumRefund);
+      expect(output[2]).to.equal(input.jrCocRefund);
+      expect(output[3]).to.equal(input.srCocRefund);
+    }
+  });
+
+  it("Checks pricePolicyCancellation extracts the accrued interest when receices MaxUint256", async () => {
+    const { uw, acMgr, rm, signer } = await helpers.loadFixture(fullSignedUWFixture);
+
+    const FULL_CANCEL_POLICY_SIGNER = getAccessManagerRole("FULL_CANCEL_POLICY_SIGNER");
+    const operationSelector = makeHashSelector("FULL_PRICE_CANCEL_POLICY");
+
+    await acMgr.setTargetFunctionRole(rm, [operationSelector], FULL_CANCEL_POLICY_SIGNER);
+    await acMgr.grantRole(FULL_CANCEL_POLICY_SIGNER, signer, 0);
+
+    const now = await helpers.time.latest();
+
+    for (let i = 0; i < 5; i++) {
+      const input = makeRandomPriceCancellationInput();
+      input.policyToCancel[10] = now - DAY;
+      input.policyToCancel[11] = now + 9 * DAY;
+      input.jrCocRefund = MaxUint256;
+      const output = await uw.pricePolicyCancellation(rm, await makeAndSignFTUWCancelInputData({ signer, ...input }));
+      expect(output.length).to.equal(4);
+      expect(output[0]).to.deep.equal(input.policyToCancel);
+      expect(output[1]).to.equal(input.purePremiumRefund);
+      expect(output[2]).not.to.equal(input.jrCocRefund);
+      expect(output[2]).to.closeTo((input.policyToCancel[8] * 9n) / 10n, 100n);
+      expect(output[3]).to.equal(input.srCocRefund);
+    }
+
+    for (let i = 0; i < 10; i++) {
+      const input = makeRandomPriceCancellationInput();
+      input.policyToCancel[10] = now - DAY;
+      input.policyToCancel[11] = now + 9 * DAY;
+      input.srCocRefund = MaxUint256;
+      const output = await uw.pricePolicyCancellation(rm, await makeAndSignFTUWCancelInputData({ signer, ...input }));
+      expect(output.length).to.equal(4);
+      expect(output[0]).to.deep.equal(input.policyToCancel);
+      expect(output[1]).to.equal(input.purePremiumRefund);
+      expect(output[2]).to.equal(input.jrCocRefund);
+      expect(output[3]).not.to.equal(input.srCocRefund);
+      expect(output[3]).to.closeTo((input.policyToCancel[9] * 9n) / 10n, 100n);
+    }
+  });
+});
