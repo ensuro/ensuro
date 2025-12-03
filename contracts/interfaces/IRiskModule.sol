bdc3931eb307d23fe8c331b6cd910b5966da6033
LucasCambon <lucambon08@gmail.com>
2025-12-03 14:42:59 -0300

    IRiskModule updated doc

--- contracts/interfaces/IRiskModule.sol
+++ contracts/interfaces/IRiskModule.sol
@@ -5,17 +5,17 @@ import {IPremiumsAccount} from "./IPremiumsAccount.sol";
 
 /**
  * @title IRiskModule interface
- * @dev Interface for RiskModule smart contracts. Gives access to RiskModule configuration parameters
+ * @notice Interface for RiskModule smart contracts. Gives access to RiskModule configuration parameters
  * @author Ensuro
  */
 interface IRiskModule {
   /**
-   * @dev Returns the address of the partner that receives the partnerCommission
+   * @notice Returns the address of the partner that receives the partnerCommission
    */
   function wallet() external view returns (address);
 
   /**
-   * @dev Returns the {PremiumsAccount} where the premiums of this risk module are collected. Never changes.
+   * @notice Returns the {PremiumsAccount} where the premiums of this risk module are collected. Never changes.
    */
   function premiumsAccount() external view returns (IPremiumsAccount);
 }
